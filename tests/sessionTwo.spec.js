4876bb0c730c1fd0084d287e8fdd4276adcf5e10
kiran <kiran.adnan@10pearls.com>
2024-10-21 19:55:50 +0500

    Updated code full-stack

--- tests/sessionTwo.spec.js
+++ tests/sessionTwo.spec.js
@@ -1,6 +1,7 @@
 const { test, expect } = require('@playwright/test');
 test('has title', async ({ page }) => {
     await page.goto('https://www.coldplay.com/');
-    await page.getByRole('button', { name: 'Sign up' });
+    await page.getByRole('button', { name: 'Accept All Cookies' }).click();
+    await page.getByRole('link', { name: 'SiGN UP', exact: true }).click();
     await expect(page.locator('h1')).toContainText('MAiLER');
 });
